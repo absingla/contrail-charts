1c8a45812443f7552a4996e4c21887ba0c8007a7
unknown <dunduvlash@gmail.com>
2017-05-01 18:53:58 +0200

    Created newui folder with a new user interface design - new icons - new colors - new markup - new linked icons

---
+++ newui/js/custom-ui.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function($) {
+  
+    //-- Activate sidebar
+    if ($('.nav-sidebar').length != 0)
+    {
+        $('.nav-sidebar').metisMenu();
+    }
+
+});
+
