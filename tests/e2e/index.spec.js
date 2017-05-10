0074a03b30861aab4632cd652df440f3e0fdcecf
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-05-10 19:36:38 +0530

    added basic jasmine test cases.

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -1,25 +1,66 @@
 var assert = require('assert');
 
 
-describe('localhost:9000', function() {
+describe('localhost:9000', function () {
 
-beforeAll(function (done) {
+  beforeAll(function (done) {
     //  elusive browser object, I will find where you're defined!
     browser
       .url('http://localhost:9000')  //  in the wdio.config.js I have the base url set to my website.
       .call(done);
   });
+  afterAll(function (done) {
+    browser.end(done);
+  });
 
 
-describe('Juniper Home', function () {
-   it('Group Clicked', function (done) {
+  describe('Juniper Home', function () {
+    it('Group Clicked', function (done) {
 
-   browser
-        .url('http://localhost:9000')
-        .getTitle().then(function (title) {
-          expect(title).toBe('Wingify');
-        }).call(done);
-   });
-});
+      browser
+        .url('http://localhost:9000');
+      var title = browser.getTitle();
+      console.log(title);
+      expect(title).toBe('Contrail Charts');
+      //browser.call(done);
+    });
+  });
+
+  describe('when a user visits index', function () {
+    it('should not have navigation visible', function (done) {
+      var opacity = browser.getCssProperty('/html/body/section/div/nav/ul/li[1]', 'opacity');
+      expect(opacity.value).toBe(1);
+      console.log(opacity);
+      browser.call(done);
+    });
+  });
+  describe('when a user clicks the menu button', function () {
+    it('should show the navigation', function (done) {
+      var opacity = browser.click('/html/body/section/div/nav/ul/li[1]').getCssProperty('/html/body/section/div/nav/ul/li[1]', 'opacity');
+      expect(opacity.value).toBe(1);
+      console.log(opacity);
+      browser.call(done);
+    });
+    it('should load Grouped Graph when click on Navigation', function (done) {
+      var opacity = browser.click('//*[@id="groupedLinks"]/li[1]/a').getCssProperty('//*[@id="cc-stacked-bar-id"]/div', 'opacity');
+      expect(opacity.value).toBe(1);
+      console.log(opacity);
+      browser.call(done);
+    });
+    it('should not load Navigation Graph when click on line 2 bar', function (done) {
+      var opacity = browser.click('//*[@id="groupedLinks"]/li[2]/a');
+      var isExisting = browser.isExisting('//*[@id="cc-stacked-bar-id"]/div');
+      console.log(isExisting);
+      expect(isExisting).toBe(false);
+      browser.call(done);
+    });
+    it('should load line 2 bar Graph when click on line 2 bar', function (done) {
+      var opacity = browser.click('//*[@id="groupedLinks"]/li[2]/a');
+      var isExisting = browser.isExisting('//*[@id="pie-chart-id-svg-wrapper"]');
+      console.log(isExisting);
+      expect(isExisting).toBe(true);
+      browser.call(done);
+    });
+  });
 
 });
