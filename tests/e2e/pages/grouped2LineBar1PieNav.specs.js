49bf733401647ac2feb74badc5a9288e37bcae88
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-05-17 21:58:22 +0530

    test on sauce lab.

--- tests/e2e/pages/grouped2LineBar1PieNav.specs.js
+++ tests/e2e/pages/grouped2LineBar1PieNav.specs.js
@@ -1,13 +1,20 @@
 /* global describe, it, expect */
 import { LogCheck } from '../../common/log.specs.js'
-// describe('Juniper Home test case for grouped2LineBar1PieNav Page', () => {
-//   it('Should not any SEVER error in console', (done) => {
-//     var newlogCheck = new LogCheck()
-//     var logIndex = newlogCheck.checkLogs('http://localhost:9000/#grouped2LineBar1PieNav')
-//     expect(logIndex).toBe(-1)
-//   })
-// })
 
-
-var logCheck = new LogCheck()
-logCheck.checkLogs('http://localhost:9000/#grouped2LineBar1PieNav', 'grouped2LineBar1PieNav')
+describe('localhost:9000', function () {
+  beforeAll(function (done) {
+    //  elusive browser object, I will find where you're defined!
+    browser
+      .url('http://localhost:9000')  //  in the wdio.config.js I have the base url set to my website.
+      .call(done);
+  });
+  afterAll(function (done) {
+    browser.end(done);
+  });
+  describe('Juniper Home test case for grouped2LineBar1PieNav Page', () => {
+    it('Should not any SEVER error in console', (done) => {
+      var logCheck = new LogCheck()
+      logCheck.checkLogs('http://localhost:9000/#grouped2LineBar1PieNav', 'grouped2LineBar1PieNav')
+    })
+  })
+});
