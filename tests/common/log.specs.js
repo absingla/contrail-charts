bcea59f5f244323eeca86534ff073ea998353b09
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-05-17 16:22:08 +0530

    common test case for console.log

--- tests/common/log.specs.js
+++ tests/common/log.specs.js
@@ -1,13 +1,17 @@
 /* global browser */
 /*  File Name : log.specs.js  common test case for check console.log and return index of logs return -1 if no error and index if any severe error   */
 export class LogCheck {
-  checkLogs (url) {
-    browser
-        .url(url)
-    var logs = browser.log('browser')
-    var index = logs.value.map(function (obj) {
-      return obj.level
-    }).indexOf('SEVERE')
-    return index
+  checkLogs(url, page_params) {
+    describe('Juniper Home test case for ' + page_params + ' Page ', () => {
+      it('Should not show any SEVER error in console', (done) => {
+        browser
+          .url(url)
+        var logs = browser.log('browser')
+        var index = logs.value.map(function (obj) {
+          return obj.level
+        }).indexOf('SEVERE')
+        expect(index).toBe(-1)
+      })
+    })
   }
 }
--- tests/e2e/pages/lineBarControls.specs.js
+++ tests/e2e/pages/lineBarControls.specs.js
@@ -1,9 +1,13 @@
 /* global describe, it, expect */
 import { LogCheck } from '../../common/log.specs.js'
-describe('Juniper Home test case for grouped2LineBar1PieNav Page', () => {
-  it('Should not any SEVER error in console', (done) => {
-    var newlogCheck = new LogCheck()
-    var logIndex = newlogCheck.checkLogs('http://localhost:9000/#grouped2LineBar1PieNav')
-    expect(logIndex).toBe(-1)
-  })
-})
+// describe('Juniper Home test case for lineBarControls Page', () => {
+//   it('Should not any SEVER error in console', (done) => {
+//     var newlogCheck = new LogCheck()
+//     var logIndex = newlogCheck.checkLogs('http://localhost:9000/#lineBarControls')
+//     expect(logIndex).toBe(-1)
+//   })
+// })
+
+
+var logCheck = new LogCheck()
+logCheck.checkLogs('http://localhost:9000/#lineBarControls', 'lineBarControls')
