92b1845b49f83fc2d628724058caeb5422573c70
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-05-17 15:25:02 +0530

    add circle yml.

--- tests/e2e/pages/legend.bar.specs.js
+++
@@ -1,9 +0,0 @@
-/* global describe, it, expect */
-import { LogCheck } from '../../common/log.specs.js'
-describe('Juniper Home test case for lineBarStackedBar Page', () => {
-  it('Should not any SEVER error in console', (done) => {
-    var newlogCheck = new LogCheck()
-    var logIndex = newlogCheck.checkLogs('http://localhost:9000/#lineBarStackedBar')
-    expect(logIndex).toBe(-1)
-  })
-})
--- wdio.conf.js
+++ wdio.conf.js
@@ -119,6 +119,7 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     // services: [],//
+    services: ['sauce'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
