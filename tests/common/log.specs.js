646dbc46702605e97d06315f0c13da364bed9ad9
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-05-18 19:08:19 +0530

    check after time out

--- tests/common/log.specs.js
+++ tests/common/log.specs.js
@@ -4,6 +4,7 @@ export class LogCheck {
   checkLogs(url, page_params) {
     browser
       .url(url)
+    browser.timeouts('implicit', 10000);
     var logs = browser.log('browser')
     console.log("logs===============================");
     console.log(logs)
--- tests/e2e/pages/lineBarControls.specs.js
+++ tests/e2e/pages/lineBarControls.specs.js
@@ -1,23 +1,23 @@
-// /* global describe, it, expect */
-// import { LogCheck } from '../../common/log.specs.js'
-// describe('localhost:9000', function () {
-//   beforeAll(function (done) {
-//     //  elusive browser object, I will find where you're defined!
-//     browser
-//       .url('http://localhost:9000')  //  in the wdio.config.js I have the base url set to my website.
-//       .call(done);
-//   });
-//   afterAll(function (done) {
-//     browser.end(done);
-//   });
-//   describe('Juniper Home test case for lineBarControls Page', () => {
-//     it('Should not any SEVER error in console', (done) => {
-//       var logCheck = new LogCheck()
-//       var index = logCheck.checkLogs('http://localhost:9000/#lineBarControls', 'lineBarControls')
-//       expect(index).toBe(-1)
-//       browser.call(done);
-//     })
-//   })
-// });  
+/* global describe, it, expect */
+import { LogCheck } from '../../common/log.specs.js'
+describe('localhost:9000', function () {
+  beforeAll(function (done) {
+    //  elusive browser object, I will find where you're defined!
+    browser
+      .url('http://localhost:9000')  //  in the wdio.config.js I have the base url set to my website.
+      .call(done);
+  });
+  afterAll(function (done) {
+    browser.end(done);
+  });
+  describe('Juniper Home test case for lineBarControls Page', () => {
+    it('Should not any SEVER error in console', (done) => {
+      var logCheck = new LogCheck()
+      var index = logCheck.checkLogs('http://localhost:9000/#lineBarControls', 'lineBarControls')
+      expect(index).toBe(-1)
+      browser.call(done);
+    })
+  })
+});  
 
 
